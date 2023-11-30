51450f361830de300814ea3ca1baf8aae2b7e299
Jordan-Finlay <>
2023-11-30 11:49:50 +0000

    added eventlistener for contentloaded and eventlistener for mouse click on buttons or submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+//Wait for DOM to finish loading before starting game
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
