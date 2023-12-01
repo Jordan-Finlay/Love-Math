fdc5f31a726aef47144963645dd751993c39081d
Jordan-Finlay <>
2023-12-01 10:59:26 +0000

    added tally scores for correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert(`Hey! You got it right! :D`);
-    } else alert (`awww.. you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!`);
-
+        incrementScore();
+    } else {
+        alert(`awww.. you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
     runGame(calculatedAnswer[1]);
 }
 
@@ -68,12 +71,20 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect scores from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
